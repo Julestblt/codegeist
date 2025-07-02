84d2885d6cecb6871ada4f418c2ced76ef4551f6
JuleZ <julestblt.dev@pm.me>
2025-07-02 20:20:01 +0200

    chore(backend: remove useless files

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,3 +1,2 @@
-export * from "./chat.routes";
 export * from "./projects.routes";
 export * from "./scan.routes";
--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,10 +1,9 @@
 import type { FastifyInstance, FastifyPluginOptions } from "fastify";
-import { chatRoutes, projectRoutes, scanRoutes } from "./";
+import { projectRoutes, scanRoutes } from "./";
 
 const apiRoutes = async (app: FastifyInstance, _opts: FastifyPluginOptions) => {
   app.register(projectRoutes, { prefix: "projects" });
   app.register(scanRoutes, { prefix: "scans" });
-  app.register(chatRoutes, { prefix: "chat" });
   app.get("/health", async () => ({ status: "ok" }));
 };
 
