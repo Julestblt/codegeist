037f0c4e410700bfd1f0d971ee1eb7042afd548c
JuleZ <julestblt.dev@pm.me>
2025-07-06 13:51:53 +0200

    feat: improve header and add footer

--- frontend-v2/src/lib/components/layout/header.svelte
+++ frontend-v2/src/lib/components/layout/header.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { Shield, Zap, Sun, Moon } from 'lucide-svelte';
 	import { Button } from '../ui/button/';
-	import { toggleMode, mode } from 'mode-watcher';
+	import { toggleMode } from 'mode-watcher';
 	import { page } from '$app/state';
 	import type { NavigationItems } from '$lib/types/navigation';
 
@@ -17,18 +17,22 @@
 	};
 </script>
 
-<header class="bg-background border-b px-6 py-4 shadow-sm">
+<header class="bg-muted border-b px-6 py-4 shadow-sm">
 	<div class="flex items-center justify-between">
-		<div class="flex cursor-pointer items-center space-x-3">
+		<a class="flex cursor-pointer items-center space-x-3" href="/">
 			<div class="relative">
-				<Shield fill={mode.current === 'dark' ? '#ffffff' : '#000000'} class="h-8 w-8 " />
-				<Zap fill="#155dfc" class="text-primary absolute -top-0.5 -right-0.5 h-4 w-4" />
+				<Shield class="h-8 w-8 text-blue-400" />
+				<Zap class="absolute -top-1 -right-1 h-4 w-4 text-yellow-400" />
 			</div>
 			<div>
-				<h1 class="bg-clip-text text-2xl font-bold">CodeGeist</h1>
+				<h1
+					class="bg-gradient-to-r from-blue-400 to-purple-400 bg-clip-text text-xl font-bold text-transparent"
+				>
+					CodeGeist
+				</h1>
 				<p class="text-muted-foreground text-xs font-medium">Code Security Analysis</p>
 			</div>
-		</div>
+		</a>
 
 		<nav class="flex items-center space-x-1">
 			{#each Object.entries(navigationItems) as [_, { label, path }]}
